80199555485d07ae41824dfa3a112aaa6064054d
Dellean Santos <dellean.santos@gmail.com>
2019-07-17 17:38:19 -0300

    prettier and test

--- index.js
+++ index.js
@@ -1,48 +1,37 @@
 const puppeteer = require('puppeteer')
 
-async function domElementScreenshot(params){
+async function domElementScreenshot(params) {
+  try {
+    //all params required
+    const url = params.url
+    const selector = params.selector
+    const launchOptions = params.launchOptions || { headless: false }
+    const setViewportOptions = params.setViewportOptions || null
+    const screenshotOptions = params.screenshotOptions || { path: 'element.png' }
+    const pageOptions = params.pageOptions || { waitUntil: 'networkidle2' }
 
-    try {
+    if (selector) {
+      const browser = await puppeteer.launch(launchOptions)
+      const browserPages = await browser.pages()
+      const browserPage = browserPages[0]
 
-        //all params required
-        const url                   = params.url
-        const selector              = params.selector
-        const launchOptions         = params.launchOptions || {headless: false }
-        const setViewportOptions    = params.setViewportOptions || null
-        const screenshotOptions     = params.screenshotOptions || { path: 'element.png' }
-        const pageOptions           = params.pageOptions || { waitUntil: 'networkidle2' }
+      if (setViewportOptions) await browserPage.setViewport(setViewportOptions)
 
-        if(selector){
+      await browserPage.goto(url, pageOptions)
+      await browserPage.waitFor(500)
+      await browserPage.waitForSelector(selector)
+      const element = await browserPage.$(selector)
+      await element.screenshot(screenshotOptions)
+      await browser.close()
 
-            const browser               = await puppeteer.launch(launchOptions)
-            const browserPages          = await browser.pages()
-            const browserPage           = browserPages[0]
-            
-            if(setViewportOptions) await browserPage.setViewport(setViewportOptions)
-            
-
-
-            await browserPage.goto(url, pageOptions)
-            await browserPage.waitFor(500)
-            await browserPage.waitForSelector(selector)
-            const element = await browserPage.$(selector)
-            await element.screenshot(screenshotOptions)
-            await browser.close()
-
-            return true
-
-        } else {
-            throw new Error('Selector is required')
-        }
-
-
-    } catch(e){
-        console.warn(e)
-        return null
-
+      return true
+    } else {
+      throw new Error('Selector is required')
     }
-
-    
+  } catch (e) {
+    console.warn(e)
+    return null
+  }
 }
 
 module.exports = domElementScreenshot
--- test.js
+++ test.js
@@ -1,12 +1,16 @@
-const domElementScreenshot = require('./index.js');
+const domElementScreenshot = require('./index.js')
 
+;(async () => {   
 
-(async () => {
-  
-    const response = await domElementScreenshot({
-        url: 'https://www.binance.com/en/trade/pro/BTC_USDT',
-        selector: '#__next div.chartContainer',
-        launchOptions: { headless: false, defaultViewport: {  width: 1200, height: 575 } },
-    })
-  
+    /*const _response = await domElementScreenshot({
+        url: 'https://www.google.com/',
+        selector: '#hplogo',
+        launchOptions: { headless: true, defaultViewport: { width: 1200, height: 575 } },
+      })*/
+
+  const response = await domElementScreenshot({
+    url: 'https://www.binance.com/en/trade/pro/BTC_USDT',
+    selector: 'div.chartContainer',
+    launchOptions: { headless: false, defaultViewport: { width: 1500, height: 1500 } },
+  })
 })()
